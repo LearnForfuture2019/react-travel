20fea2e03ae6b368f90a053f84100bb0631379ed
= <=>
2021-04-27 15:15:54 +0800

    在productImage组件中，使用Link标签来代替点击事件

--- src/components/productCollection/ProductImage.tsx
+++ src/components/productCollection/ProductImage.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Image, Typography} from "antd";
-import {withRouter, RouteComponentProps} from 'react-router-dom'
+import {withRouter, RouteComponentProps,Link} from 'react-router-dom'
 
 interface PropsType extends RouteComponentProps {
     id: string | number;
@@ -22,7 +22,7 @@ const ProductImageComponent: React.FC<PropsType> = ({
             match
 }) => {
     return (
-        <div onClick={()=>history.push(`./detail/${id}`)}>
+        <Link to={`detail/${id}`}>
             {size == "large" ? (
                 <Image src={imageSrc} height={285} width={490}/>
             ) : (
@@ -36,7 +36,7 @@ const ProductImageComponent: React.FC<PropsType> = ({
                     ¥ {price} 起
                 </Typography.Text>
             </div>
-        </div>
+        </Link>
     );
 }
 export const ProductImage = withRouter(ProductImageComponent)
