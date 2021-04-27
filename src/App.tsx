c713080fafa93ee0961e3ffcb0f56f489c6abf67
= <=>
2021-04-27 09:05:01 +0800

    添加了所有依赖

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import React from 'react';
+import logo from './logo.svg';
+import styles from "./App.module.css";
 
-const App = ()=>{
-  return <div>
-
-  </div>
+function App() {
+  return (
+    <div className={styles.App}></div>
+  );
 }
+
 export default App;
